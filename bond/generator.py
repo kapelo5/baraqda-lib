63450ac8fd98b9f717056ccce8be84f9ad2f1039
kapelo5 <77734458+kapelo5@users.noreply.github.com>
2022-04-09 18:03:37 +0200

    Update generator.py

--- bond/generator.py
+++ bond/generator.py
@@ -1,23 +1,40 @@
 import random
+import os
 from typing import List, Any
-from bond.data import PL
 
 
 class Generator:
     def __init__(self) -> None:
         self.weights: List[int] = []
         self._data: List[str] = []
-
-    def set_item(self, k: List[int], v: List[str]) -> None:
-        self.weights = k
-        self._data = v
+        self._draw: List[List[str]] = []  # cache for results
 
     def draw(self, count: int = 1) -> Any:
-        return random.choices(self._data, weights=self.weights, k=count)
+        self._draw.append(random.choices(self._data, weights=self.weights, k=count))
+
+    def generate(self, lang: str = 'PL', counter: int = 1) -> Any:
+        path = os.path.join('baraqdalib', 'data', lang)  # create path to subdir
+        filelist = []  # list where we store paths to files
+
+        for root, dirs, files in os.walk(path):  # walk and list files
+            for file in files:
+                # append the file name to the list
+                filelist.append(os.path.join(root, file))
 
 
-def generate(counter: int = 1, lang: str = 'PL') -> List[str]:
-    gen = Generator()
-    if lang == 'PL':  # Task: create request without checking if
-        gen.set_item(list(PL.StoreData.first_name_male_weighs), sorted(list(PL.StoreData.first_name_male)))
-    return gen.draw(counter)
+        for filepath in filelist:  # read files and store data from them
+            file = open(filepath, 'r', encoding='utf-8')
+            lines = file.readlines()
+            for line in lines:
+                line = line.strip('\n\r')
+                temp_tab = line.split(" ")
+                self._data.append(temp_tab[0])
+                self.weights.append(int(temp_tab[1]))
+                self.draw(counter)
+
+
+
+        return self._draw
+
+    def stored_draw(self) -> list[list[str]]:
+        return self._draw
