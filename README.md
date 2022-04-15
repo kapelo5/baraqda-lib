51e57f0191b70a02305d722f2610fdb0246937a0
kapel0 <aleksander.karbowy@gmail.com>
2022-04-15 12:16:41 +0200

    Repair tests. Change README.md

--- README.md
+++ README.md
@@ -7,6 +7,6 @@ State: PoC
 # How to run
 
 After downloading folder, install libraries from requirements.txt
-
+    > pip install -r requirements.txt
 Run example.py
-``python3 example.py``
+    > python3 example.py
--- tests/test.py
+++ tests/test.py
@@ -1,49 +1,36 @@
 import unittest
 from collections import Counter
-from numpy import cumsum
-# from baraqdalib import Generator
-# from baraqda-lib.baraqdalib import Generator
+from baraqdalib import Generator
+
 
 class MyTestCase(unittest.TestCase):
-    # def __init__(self):
-    #     self.p = Generator()
-
-    # def read_test_file(self):
-    #     file = open('test_file.txt', 'r')
-    #     lines =
-
-        # wziąc  i wczytać tuataj testowy pliczek i potem na tych danych sprawdzać resztę funkjci
-        # powołanie instancji klasy jezt bez sensu, precież ja chcę ttylko sprawdzić jak i czy funkcje w klasie są sprawne
-        # czyli omijamy powołanie klasy i sprawdzamy tylko czy pojedynczaa funkcja działa
-        # jeśli kazda funkcja podrzędna działą to główna też będzie działała
-        # to nie działa tak jak bym myslał, potrzebuje obiektu jako self do przekaznia do funkcji, inaczej
-        # przesuwa mi lang do self i wtedy nie działa mi stworzenie ścieżki
-
 
     def test_first_name(self):
-
-        name = Generator.generate(Generator(),'PL', 1)
-        print(name)
+        p = Generator()
+        p.weights = [[10, 20, 100, 50, 5]]
+        p._data = [['Szymon', 'Marcin', 'Jan', 'Jakub', 'Wiktor']]
+        name = p.generate('PL', 1)[0][0]
         assert name
-        # self.assertIsInstance(name, str)
-        # assert name in p._data
+        self.assertIsInstance(name, str)
+        self.assertIn(name, p._data[0])
 
     def test_weighed_random(self):
         p = Generator()
-        names = p.generate('PL', 1000)
-        print(names)
+        p.weights = [[10, 20, 100, 50, 5]]
+        p._data = [['Jakub', 'Jan', 'Marcin', 'Szymon', 'Wiktor']]
+        weights_in_percent = []
+        names = p.generate('PL', 10000)[0]
         names_counts = Counter(names)
         names_count_az = {}
         for i in sorted(names_counts):
             names_count_az[i] = names_counts[i]
         names_counts_az_weighs = list(names_count_az.values())
         for i in range(len(names_counts_az_weighs)):
-            names_counts_az_weighs[i] = float(int(names_counts_az_weighs[i])/1000)
-
-        names_counts_az_weighs = cumsum(names_counts_az_weighs)
+            names_counts_az_weighs[i] = round(int(names_counts_az_weighs[i])/10000, 3)
+            weights_in_percent.append(round(p.weights[0][i]/sum(p.weights[0]), 3))
 
         for i in range(len(names_counts_az_weighs)):
-            self.assertAlmostEqual(names_counts_az_weighs[i], p.weights[i], delta=0.05)
+            self.assertAlmostEqual(names_counts_az_weighs[i], weights_in_percent[i], delta=0.02)
 
 
 if __name__ == '__main__':
