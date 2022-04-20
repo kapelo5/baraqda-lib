f1d9724d4e626f1366faba8a7560cd87adc071f8
kapel0 <aleksander.karbowy@gmail.com>
2022-04-21 00:24:54 +0200

    Change storing data for nested dict

--- baraqdalib/generator.py
+++ baraqdalib/generator.py
@@ -1,52 +1,56 @@
 import random
 import os
-from typing import List
+from typing import List, Dict
 
 
 class Generator:
     def __init__(self) -> None:
-        self.weights: List[List[int]] = []  # weights of stored data
-        self._data: List[List[str]] = []  # storing data
-        self._draw: List[List[str]] = []  # cache for results
-        self.filelist: List[str] = []  # list of files that are find
+        self._data: Dict[str, Dict[str, Dict[str, list]]] = dict(dict(dict()))
 
-    def draw(self, count: int = 1) -> None:  # create table with weighted draw and save to cache
-        for i in range(len(self._data)):
-            self._draw.append(random.choices(self._data[i], weights=self.weights[i], k=count))
+    def draw(self, lang: str, data_type: str, count: int = 1) -> List[str]:  # create table with weighted draw
+        return random.choices(self._data[lang][data_type]['values'],
+                              weights=self._data[lang][data_type]['weights'],
+                              k=count)
 
-    def search_files(self, path) -> None:
+    def search_files(self, path, sep, lang) -> None:
         for root, dirs, files in os.walk(path):  # walk and list files
             for file in files:
-                # append the file name to the list
-                self.filelist.append(os.path.join(root, file))
+                # make keys in _data for files
+                filename = file.split('.')[0]
+                self._data[lang][filename] = {}  # create dict with key filename
+                self.read_files(os.path.join(root, file), sep, lang, filename)
 
-    def read_files(self) -> None:
-        for filepath in self.filelist:  # read files and store data from them
-            file = open(filepath, 'r', encoding='utf-8')  # open file
-            lines = file.readlines()  # read all lines
-            readed_data: List[str] = []
-            readed_weights: List[int] = []
-            file.close()  # close file
-            for line in lines:  # read line by line and prepare to saving in class variables
-                line = line.strip('\n\r')
-                temp_tab = line.split(" ")
-                readed_data.append(temp_tab[0])
-                readed_weights.append(int(temp_tab[1]))
-            # save readed data to class variables
-            self._data.append(readed_data)
-            self.weights.append(readed_weights)
+    def read_files(self, filepath, separator, lang, filename) -> None:
+        # read files and store data from them
+        file = open(filepath, 'r', encoding='utf-8')  # open file
+        lines = file.readlines()  # read all lines
+        file.close()  # close file
+        # make temp list to store data from file
+        values: List[str] = []
+        weights: List[int] = []
+        for line in lines[1::]:  # read line by line and prepare to saving in class variables start from 1
+            line = line.strip('\n\r')
+            temp_tab = line.split(separator)
+            values.append(temp_tab[0])
+            weights.append(int(temp_tab[1]))
 
+        keys = lines[0].strip('\n\r').split(separator)  # read keys
+        self._data[lang][filename][keys[0]] = values
+        self._data[lang][filename][keys[1]] = weights
 
-    def generate(self, lang: str = 'PL', counter: int = 1) -> List[List[str]]:
-        if len(self._draw) == 0:
+    def generate(self, lang: str, data_type: str, counter: int = 1, sep: str = ' ') -> List[str]:
+        # check if key lang exist
+        try:
+            self._data[lang]
+        except KeyError:
             path = os.path.join('baraqdalib', 'data', lang)  # create path to subdirectories
-            self.search_files(path)  # search files in directory
-            self.read_files()   # read files
-            self.draw(counter)  # generate weighted dataw
-        else:
-            self._draw = []  # clean data from last draw
-            self.draw(counter)  # generate weighted dataw
-        return self._draw
+            self._data[lang] = {}
+            self.search_files(path, sep, lang)  # search files in directory
+        else:  # check if key data_type exist
+            try:
+                self._data[lang][data_type]
+            except KeyError:
+                path = os.path.join('baraqdalib', 'data', lang)  # create path to subdirectories
+                self.search_files(path, sep, lang)  # search files in directory
 
-    def stored_draw(self) -> List[List[str]]:  # return stored generated data
-        return self._draw
+        return self.draw(lang, 'male_names', counter)  # generate weighted dataw
