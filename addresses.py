24235f64d56d251631eb72de25306eb61facafa6
SecGuru2 <rogonm2o@gmail.com>
2022-04-22 21:33:47 +0200

    Working city and street generation

---
+++ addresses.py
@@ -0,0 +1,54 @@
+from baraqdalib import Generator
+import os
+import json
+import csv
+import random
+
+
+class Addresses:
+    def __init__(self):
+        self.address_generator = Generator()
+        self.streets_file_name: str = 'streets.csv'
+        self.cities_file_name: str = 'cities.json'
+        self.cities_pops_data: str = 'cities_pops'
+        self.cities_pops_file_name: str = 'cities_pops.txt'
+        self.lang = 'PL'
+        self.sep: str = '\t'
+
+    def generate(self, counter: int = 1):
+        generated_cities = self.address_generator.generate(self.lang, self.cities_pops_data, counter, self.sep)
+        return generated_cities
+
+    def getPath(self, file_name):
+        return os.path.join('baraqdalib', 'addressData', file_name)
+
+    def getSymCity(self, city):
+        with open(self.getPath(self.cities_file_name), 'r', encoding='utf-8-sig') as citiesFile:
+            cities = json.load(citiesFile)
+            for sym in cities:
+                if cities[sym]['NAZWA'] == city:
+                    return cities[sym]['SYM']
+        return 'No city in file'
+
+    def getStreets(self, city_sym):
+        streets = []
+        streets_dump = []
+        with open(self.getPath(self.streets_file_name), 'r', encoding='utf-8-sig') as streetsFile:
+            streets_csv = csv.reader(streetsFile)
+            for row in streets_csv:
+                if row[4] == city_sym:
+                    streets.append((row[6] + ' ' + row[7] + ' ' + row[8]).replace('  ', ' '))
+                else:
+                    streets_dump.append((row[6] + ' ' + row[7] + ' ' + row[8]).replace('  ', ' '))
+            if not streets:
+                return random.choice(streets_dump)
+            return random.choice(streets)
+
+
+addr = Addresses()
+cities = addr.generate(1000)
+print(cities[0])
+sym = addr.getSymCity(cities[0])
+print(sym)
+streets1 = addr.getStreets(sym)
+print(streets1)
