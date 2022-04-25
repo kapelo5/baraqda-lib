6eba2ecd36586e15b700d7cfe7d0fc4914469315
SecGuru2 <rogonm2o@gmail.com>
2022-04-25 17:39:17 +0200

    Added cities.csv deleted cities.json and multiple word cities

--- baraqdalib/addresses.py
+++ baraqdalib/addresses.py
@@ -1,6 +1,5 @@
 from baraqdalib import Generator
 import os
-import json
 import csv
 import random
 
@@ -9,7 +8,7 @@ class Addresses:
     def __init__(self):
         self.address_generator = Generator()
         self.streets_file_name: str = 'streets.csv'
-        self.cities_file_name: str = 'cities.json'
+        self.cities_file_name: str = 'cities.csv'
         self.cities_pops_data: str = 'cities_pops'
         self.cities_pops_file_name: str = 'cities_pops.txt'
         self.lang = 'PL'
@@ -22,24 +21,29 @@ class Addresses:
     def getPath(self, file_name):
         return os.path.join('baraqdalib', 'addressData', file_name)
 
-    def getSymCity(self, city):
-        with open(self.getPath(self.cities_file_name), 'r', encoding='utf-8-sig') as citiesFile:
-            cities = json.load(citiesFile)
+    def getSymCity(self, city: str):
+        # print(city)
+        with open(os.path.join('baraqdalib', 'addressData', self.cities_file_name), 'r', encoding='utf-8-sig') as citiesFile:
+            cities = csv.reader(citiesFile, delimiter=';')
             for sym in cities:
-                if cities[sym]['NAZWA'] == city:
-                    return cities[sym]['SYM']
+                if sym[1] == city:
+                    return sym[0]
         return 'No city in file'
 
-    def getStreets(self, city_sym):
-        streets = []
-        streets_dump = []
-        with open(self.getPath(self.streets_file_name), 'r', encoding='utf-8-sig') as streetsFile:
-            streets_csv = csv.reader(streetsFile)
-            for row in streets_csv:
-                if row[4] == city_sym:
-                    streets.append((row[6] + ' ' + row[8] + ' ' + row[7]).replace('  ', ' '))
-                else:
-                    streets_dump.append((row[6] + ' ' + row[8] + ' ' + row[7]).replace('  ', ' '))
-            if not streets:
-                return random.choice(streets_dump)
-            return random.choice(streets)
+    def getStreets(self, city_sym: int):
+        if city_sym != 'No city in file':
+            streets = []
+            streets_dump = []
+            with open(os.path.join('baraqdalib', 'addressData', self.streets_file_name), 'r', encoding='utf-8-sig') as streetsFile:
+                streets_csv = csv.reader(streetsFile)
+                for row in streets_csv:
+                    if row[4] == city_sym:
+                        streets.append((row[6] + ' ' + row[8] + ' ' + row[7]).replace('  ', ' '))
+                    elif row[4] != 'SYM':
+                        if 287400 % int(row[4]):
+                            streets_dump.append((row[6] + ' ' + row[8] + ' ' + row[7]).replace('  ', ' '))
+                if not streets:
+                    return random.choice(streets_dump), (round(random.lognormvariate(1.6, 2)) + 1) % 200
+                return random.choice(streets), (round(random.lognormvariate(1.6, 2)) + 1) % 200
+        else:
+            return city_sym
--- example_addresses.py
+++ example_addresses.py
@@ -1,9 +1,14 @@
 from baraqdalib.addresses import Addresses
 
 addr = Addresses()
-cities = addr.generate(10)
+cities = addr.generate(100)
 for i in cities:
     print(i)
     sym = addr.getSymCity(i)
     streets1 = addr.getStreets(sym)
     print(streets1)
+
+sym = addr.getSymCity('Mińsk Mazowiecki')
+print(sym)
+streets1 = addr.getStreets(sym)
+print(streets1)
